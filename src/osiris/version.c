3a84cecad80d3559f054e7ceca37acc0a21d88e6
ermiry <erick_salas@ermiry.com>
2020-12-07 12:57:17 -0600

    Refactored version methods

--- src/osiris/version.c
+++ src/osiris/version.c
@@ -1,26 +1,26 @@
-#include <cerver/utils/log.h>
+#include <stdio.h>
 
 #include "osiris/version.h"
 
 // print full osiris version information
 void osiris_version_print_full (void) {
 
-	cerver_log_msg ("\n\nOsiris Mini Version: %s\n", OSIRIS_MINI_VERSION_NAME);
-	cerver_log_msg ("Release Date & time: %s - %s\n", OSIRIS_MINI_VERSION_DATE, OSIRIS_MINI_VERSION_TIME);
-	cerver_log_msg ("Author: %s\n\n", OSIRIS_MINI_VERSION_AUTHOR);
+	(void) printf ("\n\nOsiris Mini Version: %s\n", OSIRIS_MINI_VERSION_NAME);
+	(void) printf ("Release Date & time: %s - %s\n", OSIRIS_MINI_VERSION_DATE, OSIRIS_MINI_VERSION_TIME);
+	(void) printf ("Author: %s\n\n", OSIRIS_MINI_VERSION_AUTHOR);
 
 }
 
 // print the version id
 void osiris_version_print_version_id (void) {
 
-	cerver_log_msg ("\n\nOsiris Mini Version ID: %s\n", OSIRIS_MINI_VERSION);
+	(void) printf ("\n\nOsiris Mini Version ID: %s\n", OSIRIS_MINI_VERSION);
 
 }
 
 // print the version name
 void osiris_version_print_version_name (void) {
 
-	cerver_log_msg ("\n\nOsiris Mini Version: %s\n", OSIRIS_MINI_VERSION_NAME);
+	(void) printf ("\n\nOsiris Mini Version: %s\n", OSIRIS_MINI_VERSION_NAME);
 
 }
