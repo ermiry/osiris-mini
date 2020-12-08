96b4238a5188a89f9536d0911eb147251f2124ed
ermiry <erick_salas@ermiry.com>
2020-12-08 09:40:59 -0600

    Updated version time & date

--- include/osiris/version.h
+++ include/osiris/version.h
@@ -3,8 +3,8 @@
 
 #define OSIRIS_MINI_VERSION					"0.1"
 #define OSIRIS_MINI_VERSION_NAME			"Version 0.1"
-#define OSIRIS_MINI_VERSION_DATE			"06/12/2020"
-#define OSIRIS_MINI_VERSION_TIME			"23:08 CST"
+#define OSIRIS_MINI_VERSION_DATE			"08/12/2020"
+#define OSIRIS_MINI_VERSION_TIME			"09:40 CST"
 #define OSIRIS_MINI_VERSION_AUTHOR			"Erick Salas"
 
 // print full osiris version information
